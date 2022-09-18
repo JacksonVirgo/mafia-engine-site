f76bf5f9027c236fbaef80f288b5567a4f62b65a
Jack Nash <jacknash42@gmail.com>
2022-09-19 00:33:20 +1000

    debug

--- src/pages/api/discord/interaction.ts
+++ src/pages/api/discord/interaction.ts
@@ -17,10 +17,12 @@ type Error = {
 	error: string;
 };
 export default (req: NextApiRequest, res: NextApiResponse<DiscordInteraction | Error>) => {
-	console.log('Body', req.body, '\n\nHeaders', req.headers);
 	const signature = req.headers['X-Signature-Ed25519'] as string;
 	const timestamp = req.headers['X-Signature-Timestamp'] as string;
 	const body = JSON.stringify(req.body);
+
+	console.log(signature, timestamp, body, PUBLIC_KEY);
+
 	const isValidRequest = verifyKey(body, signature, timestamp, PUBLIC_KEY);
 
 	if (!isValidRequest) return res.status(401).json({ error: 'Unauthorized' });
