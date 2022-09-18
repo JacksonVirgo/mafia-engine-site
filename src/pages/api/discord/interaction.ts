07d17d80532dcd880a7468570bc786cc8c608f2c
Jack Nash <jacknash42@gmail.com>
2022-09-18 21:51:36 +1000

    add auth check discord interaction edge

--- src/pages/api/discord/interaction.ts
+++ src/pages/api/discord/interaction.ts
@@ -1,4 +1,8 @@
 import { NextRequest } from 'next/server';
+import { verifyKey } from 'discord-interactions';
+
+const PUBLIC_KEY = '843514276383031296';
+
 export const config = {
 	runtime: 'experimental-edge',
 };
@@ -8,6 +12,20 @@ type DiscordInteraction = {
 	data?: string;
 };
 export default (req: NextRequest) => {
+	const signature = req.headers.get('X-Signature-Ed25519') as unknown as string;
+	const timestamp = req.headers.get('X-Signature-Timestamp') as unknown as string;
+	const body: string = req.body as unknown as string;
+	const isValidRequest = verifyKey(body, signature, timestamp, PUBLIC_KEY);
+
+	if (!isValidRequest) {
+		return new Response('Bad request signature', {
+			status: 401,
+			headers: {
+				'content-type': 'application/json',
+			},
+		});
+	}
+
 	const interactionResponse: DiscordInteraction = { type: 1, data: 'PING' };
 	return new Response(JSON.stringify(interactionResponse), {
 		status: 200,
