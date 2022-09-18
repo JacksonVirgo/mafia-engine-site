d7fe2d6d6d44c72ed241b2ef45cca74e6e47859e
Jack Nash <jacknash42@gmail.com>
2022-09-18 22:21:52 +1000

    debug

--- src/pages/api/discord/interaction.ts
+++ src/pages/api/discord/interaction.ts
@@ -4,20 +4,26 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const PUBLIC_KEY = '843514276383031296';
 
-export const config = {
-	runtime: 'experimental-edge',
-};
+// export const config = {
+// 	runtime: 'experimental-edge',
+// };
 
 type DiscordInteraction = {
 	type: number;
 	data?: string;
 };
-export default (req: NextApiRequest, res: NextApiResponse<DiscordInteraction | string>) => {
+
+type Error = {
+	error: string;
+};
+export default (req: NextApiRequest, res: NextApiResponse<DiscordInteraction | Error>) => {
+	console.log('Body', req.body, '\n\nHeaders', req.headers);
 	const signature = req.headers['X-Signature-Ed25519'] as string;
 	const timestamp = req.headers['X-Signature-Timestamp'] as string;
 	const body: string = req.body;
 	const isValidRequest = verifyKey(body, signature, timestamp, PUBLIC_KEY);
-	if (!isValidRequest) return res.status(401).send('Bad request signature');
+
+	if (!isValidRequest) return res.status(401).json({ error: 'Unauthorized' });
 	const interactionResponse: DiscordInteraction = { type: 1, data: 'PING' };
 	return res.status(200).json(interactionResponse);
 };
