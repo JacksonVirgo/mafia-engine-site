99f9d6fc5519881d046caa63dfc9745487efeb6b
Jack Nash <jacknash42@gmail.com>
2022-09-18 22:14:07 +1000

    added nextapiresponse

--- src/pages/api/discord/interaction.ts
+++ src/pages/api/discord/interaction.ts
@@ -1,37 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyKey } from 'discord-interactions';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const PUBLIC_KEY = '843514276383031296';
 
-// export const config = {
-// 	runtime: 'experimental-edge',
-// };
+export const config = {
+	runtime: 'experimental-edge',
+};
 
 type DiscordInteraction = {
 	type: number;
 	data?: string;
 };
-export default (req: NextRequest) => {
-	console.log(req.body, req.headers);
-	const signature = req.headers.get('X-Signature-Ed25519') as unknown as string;
-	const timestamp = req.headers.get('X-Signature-Timestamp') as unknown as string;
-	const body: string = req.body as unknown as string;
+export default (req: NextApiRequest, res: NextApiResponse<DiscordInteraction | string>) => {
+	console.log(req);
+	const signature = req.headers['X-Signature-Ed25519'] as string;
+	const timestamp = req.headers['X-Signature-Timestamp'] as string;
+	const body: string = req.body;
 	const isValidRequest = verifyKey(body, signature, timestamp, PUBLIC_KEY);
-
-	if (!isValidRequest) {
-		return new Response('Bad request signature', {
-			status: 401,
-			headers: {
-				'content-type': 'application/json',
-			},
-		});
-	}
-
+	if (!isValidRequest) return res.status(401).send('Bad request signature');
 	const interactionResponse: DiscordInteraction = { type: 1, data: 'PING' };
-	return new Response(JSON.stringify(interactionResponse), {
-		status: 200,
-		headers: {
-			'content-type': 'application/json',
-		},
-	});
+	return res.status(200).json(interactionResponse);
 };
