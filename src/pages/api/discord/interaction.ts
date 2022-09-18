1af01cd9b85becc521dd36af4ea7af5ed555330f
Jack Nash <jacknash42@gmail.com>
2022-09-19 01:03:25 +1000

    test interaction reply

--- src/pages/api/discord/interaction.ts
+++ src/pages/api/discord/interaction.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyKey } from 'discord-interactions';
 import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
 const PUBLIC_KEY = '67bc59e4adb1d0ce097926d04244d7eff090b16277572a147f5e9a25d9684890';
 
@@ -16,17 +17,28 @@ type DiscordInteraction = {
 type Error = {
 	error: string;
 };
-export default (req: NextApiRequest, res: NextApiResponse<DiscordInteraction | Error>) => {
+export default async (req: NextApiRequest, res: NextApiResponse<DiscordInteraction | Error>) => {
+	const { body } = req;
 	const signature = req.headers['X-Signature-Ed25519'.toLowerCase()] as string;
 	const timestamp = req.headers['X-Signature-Timestamp'.toLowerCase()] as string;
-	const body = JSON.stringify(req.body);
+	const cleanBody = JSON.stringify(req.body);
 
-	console.log(req.body);
-	console.log(`${signature}\n${timestamp}\n${PUBLIC_KEY}`);
+	const interactionID = body.id;
+	if (!interactionID) {
+		const isValidRequest = verifyKey(cleanBody, signature, timestamp, PUBLIC_KEY);
+		if (!isValidRequest) return res.status(401).json({ error: 'Unauthorized' });
+		const interactionResponse: DiscordInteraction = { type: 1, data: 'PING' };
+		return res.status(200).json(interactionResponse);
+	}
 
-	const isValidRequest = verifyKey(body, signature, timestamp, PUBLIC_KEY);
+	const { token, data, member } = body;
 
-	if (!isValidRequest) return res.status(401).json({ error: 'Unauthorized' });
-	const interactionResponse: DiscordInteraction = { type: 1, data: 'PING' };
-	return res.status(200).json(interactionResponse);
+	await axios.post(`https://discord.com/v10/interactions/${interactionID}/${token}/callback`, {
+		type: 4,
+		data: {
+			content: 'This is from a serverless application. Hoorah!',
+		},
+	});
+
+	// TEST DEFER
 };
