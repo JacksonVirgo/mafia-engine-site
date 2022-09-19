180f144eb26399b71617af26f5a972309144f496
Jack Nash <jacknash42@gmail.com>
2022-09-20 06:51:42 +1000

    added basic interaction response

--- src/pages/api/discord/interaction.ts
+++ src/pages/api/discord/interaction.ts
@@ -1,15 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { verifyKey } from 'discord-interactions';
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
-
-const PUBLIC_KEY = '67bc59e4adb1d0ce097926d04244d7eff090b16277572a147f5e9a25d9684890';
 
 // export const config = {
 // 	runtime: 'experimental-edge',
 // };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	console.log(`--- DATA START ---\n${req.body}\n--- DATA END ---`);
-	return res.status(200).json({});
+	const { body } = req;
+	const { command } = body;
+
+	if (!command) return res.status(400).send('Invalid Body.');
+	const { application_id, token } = command;
+	const discordURI = `https://discord.com/api/v10/webhooks/${application_id}/${token}/messages/@original`;
+
+	console.log(discordURI);
+
+	const response = await axios.patch(discordURI, {
+		content: 'Response handled by Vercel.',
+	});
+
+	console.log(response);
+
+	return res.status(200).json(response);
 };
