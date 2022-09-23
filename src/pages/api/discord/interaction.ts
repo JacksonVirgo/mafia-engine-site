06835eaaced55f5e4bda18aaad7f64e3ce18209e
Jack Nash <jacknash42@gmail.com>
2022-09-24 03:04:45 +1000

    added basic interaction response

--- src/pages/api/discord/interaction.ts
+++ src/pages/api/discord/interaction.ts
@@ -6,9 +6,13 @@ import axios from 'axios';
 // };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	console.log(req.body);
-
-	console.log(req.body.options._hoistedOptions);
-
-	return res.status(200).json({});
+	const { body } = req;
+	const { command } = body;
+	if (!command) return res.status(400).send('Invalid Body.');
+	const { application_id, token } = command;
+	const discordURI = `https://discord.com/api/v10/webhooks/${application_id}/${token}/messages/@original`;
+	const response = await axios.patch(discordURI, {
+		content: 'Response handled by Vercel.',
+	});
+	return res.status(200).json(response);
 };
