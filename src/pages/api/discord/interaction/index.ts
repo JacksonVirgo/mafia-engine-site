543ec76b7bcc73969514fd9e5ab55e5e868fb394
Jack Nash <jacknash42@gmail.com>
2022-09-24 03:14:54 +1000

    added vote discord endpoint

---
+++ src/pages/api/discord/interaction/index.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+
+// export const config = {
+// 	runtime: 'experimental-edge',
+// };
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+	const { body } = req;
+	const { command } = body;
+	if (!command) return res.status(400).send('Invalid Body.');
+	const { application_id, token } = command;
+	const discordURI = `https://discord.com/api/v10/webhooks/${application_id}/${token}/messages/@original`;
+	const response = await axios.patch(discordURI, {
+		content: 'Response handled by Vercel.',
+	});
+	return res.status(200).json(response);
+};
