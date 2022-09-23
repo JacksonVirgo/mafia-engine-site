d7ea82364acdd0935cf57aa7bede4b02b2cf26be
Jack Nash <jacknash42@gmail.com>
2022-09-24 01:51:33 +1000

    update logs

--- src/pages/api/discord/interaction.ts
+++ src/pages/api/discord/interaction.ts
@@ -6,24 +6,9 @@ import axios from 'axios';
 // };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const { body } = req;
-	const { command } = body;
+	console.log(req.body);
 
-	console.log(body);
+	console.log(req.body.options._hoistedOptions);
 
 	return res.status(200).json({});
-
-	// if (!command) return res.status(400).send('Invalid Body.');
-	// const { application_id, token } = command;
-	// const discordURI = `https://discord.com/api/v10/webhooks/${application_id}/${token}/messages/@original`;
-
-	// console.log(discordURI);
-
-	// const response = await axios.patch(discordURI, {
-	// 	content: 'Response handled by Vercel.',
-	// });
-
-	// console.log(response);
-
-	// return res.status(200).json(response);
 };
